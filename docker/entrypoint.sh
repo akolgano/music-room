4cc7c08c1391a529745b6ea8e2c11a0c65d9b980
akolgano <anna080688@gmail.com>
2025-04-15 21:37:22 +0800

    docker setup + 2 basic API from deezer

---
+++ docker/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+mkdir -p /var/log
+
+: > /var/log/django.log
+
+echo "$(date): Starting the Django service..." >> /var/log/django.log
+
+echo "Waiting for database..."
+while ! nc -z $POSTGRES_HOST 5432; do
+  sleep 1
+done
+export PYTHONPATH=/app
+
+echo "Applying migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+SUPERUSER_EXISTS=$(python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists())")
+if [ "$SUPERUSER_EXISTS" = "False" ]; then
+  echo "Creating superuser..."
+  python manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL"
+else
+  echo "Superuser already exists."
+fi
+
+
+# echo "Starting Gunicorn server..."
+# exec gunicorn pong.wsgi:application --bind 0.0.0.0:8000
+
+
+echo "Starting development server..."
+exec python manage.py runserver 0.0.0.0:8000
