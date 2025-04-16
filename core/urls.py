ca75e22c747d9f419a36617ee06aeb3c1e84b7fc
akolgano <anna080688@gmail.com>
2025-04-16 14:00:37 +0800

    add users

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('deezer/', include('apps.deezer.urls')), 
+    path('deezer/', include('apps.deezer.urls')),
+    path('users/', include('apps.users.urls')), 
 ]
