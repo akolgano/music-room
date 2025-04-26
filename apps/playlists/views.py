0fa80881f1471ea73c84d2011693d0ec0ac7ab53
akolgano <anna080688@gmail.com>
2025-04-26 17:28:38 +0800

    3 playlist api

--- apps/playlists/views.py
+++ apps/playlists/views.py
@@ -1,11 +1,35 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes
 from django.http import JsonResponse
 from .models import Playlist
 from apps.tracks.models import Track
 from django.shortcuts import get_object_or_404
+from rest_framework.authentication import TokenAuthentication
+
+@api_view(['POST'])
+#@authentication_classes([TokenAuthentication])
+def create_new_playlist(request):
+    user = request.user
+    name = request.data.get('name')
+    description = request.data.get('description', '')
+    public = request.data.get('public', False)
+    if not name:
+        return JsonResponse({"error": "Playlist name is required."}, status=400)
+    playlist = Playlist.objects.create(
+        name=name,
+        description=description,
+        public=public,
+        creator=user
+    )
+    user.saved_playlists.add(playlist)
+
+    return JsonResponse({
+        "message": "Empty playlist is created.",
+        "playlist_id": playlist.id
+    }, status=201)
 
 
 @api_view(['GET'])
+#@authentication_classes([TokenAuthentication])
 def get_user_saved_playlists(request):
     user = request.user
 
@@ -28,6 +52,7 @@ def get_user_saved_playlists(request):
 
 
 @api_view(['GET'])
+#@authentication_classes([TokenAuthentication])
 def get_all_shared_playlists(request):
 
     playlists = Playlist.objects.filter(public=True)
@@ -49,6 +74,7 @@ def get_all_shared_playlists(request):
 
 
 @api_view(['POST'])
+#@authentication_classes([TokenAuthentication])
 def save_shared_playlist(request):
     if not request.user.is_authenticated:
         return JsonResponse({"error": "Authentication required."}, status=401)
@@ -86,6 +112,7 @@ def save_shared_playlist(request):
 
 
 @api_view(['POST'])
+#@authentication_classes([TokenAuthentication])
 def add_track_to_playlist(request, playlist_id, track_id):
     try:
         playlist = get_object_or_404(Playlist, id=playlist_id)
@@ -102,3 +129,53 @@ def add_track_to_playlist(request, playlist_id, track_id):
         return JsonResponse({"error": "Playlist not found."}, status=404)
     except Track.DoesNotExist:
         return JsonResponse({"error": "Track not found."}, status=404)
+
+@api_view(['POST'])
+#@authentication_classes([TokenAuthentication])
+def add_items_to_playlist(request, playlist_id):
+    try:
+        user = request.user
+        playlist = get_object_or_404(Playlist, id=playlist_id)
+        track_ids = request.data.get('track_ids', [])  
+        tracks = Track.objects.filter(id__in=track_ids)
+
+        if tracks.exists():
+            playlist.tracks.add(*tracks)
+        else:
+            return JsonResponse({"error": "One or more tracks not found."}, status=404)
+
+        user.saved_playlists.add(playlist)
+        return JsonResponse({
+            "message": "Tracks added successfully.",
+            "playlist_id": playlist.id,
+            "tracks": [track.id for track in tracks]
+        }, status=201)
+    except Playlist.DoesNotExist:
+        return JsonResponse({"error": "Playlist not found."}, status=404)
+    except Track.DoesNotExist:
+        return JsonResponse({"error": "Track not found."}, status=404)
+
+
+@api_view(['GET'])
+#@authentication_classes([TokenAuthentication])
+def get_playlist(request, playlist_id):
+    try:
+        user = request.user
+        playlist = get_object_or_404(Playlist, id=playlist_id)
+
+
+        playlist_data = []
+        tracks = playlist.tracks.all()
+        track_list = [{'name': track.name, 'artist': track.artist} for track in tracks]
+
+        playlist_data.append({
+            'name': playlist.name,
+            'description': playlist.description,
+            'public': playlist.public,
+            'creator': playlist.creator.username,
+            'tracks': track_list,
+        })
+
+        return JsonResponse({'playlist': playlist_data})
+    except Playlist.DoesNotExist:
+        return JsonResponse({"error": "Playlist not found."}, status=404)
