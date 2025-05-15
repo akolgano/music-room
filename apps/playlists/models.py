20eefe3a5bd9d4b4122475dd5b1dddd0eb077635
akolgano <anna080688@gmail.com>
2025-05-15 12:03:38 +0800

    move tracks in playlist api

--- apps/playlists/models.py
+++ apps/playlists/models.py
@@ -11,8 +11,24 @@ class Playlist(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     public = models.BooleanField(default=True)
-    tracks = models.ManyToManyField(Track, related_name='playlists')
+    #tracks = models.ManyToManyField(Track, related_name='playlists')
     users_saved = models.ManyToManyField(User, related_name='saved_playlists', blank=True)
 
     def __str__(self):
         return self.name
+
+class PlaylistTrack(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE, related_name='tracks')
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
+    position = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('playlist', 'position')
+        ordering = ['position']
+
+
+    def save(self, *args, **kwargs):
+        if not self.position:
+            # Automatically set position to the next available integer
+            self.position = PlaylistTrack.objects.filter(playlist=self.playlist).count()
+        super().save(*args, **kwargs)
--- apps/playlists/urls.py
+++ apps/playlists/urls.py
@@ -3,11 +3,13 @@ from . import views
 
 urlpatterns = [
     path('playlists', views.create_new_playlist, name='playlists'),
-    path('playlists/<int:playlist_id>', views.get_playlist, name='get_playlist'), 
+    path('playlists/<int:playlist_id>', views.get_playlist_info, name='get_playlist'), 
     path('playlists/<int:playlist_id>/tracks', views.add_items_to_playlist, name='add_items'),
     path('playlists/<int:playlist_id>/remove_tracks', views.remove_playlist_items, name='remove_items'),
-    path('save_playlist/', views.save_shared_playlist, name='save_playlist'),
+    #path('save_playlist/', views.save_shared_playlist, name='save_playlist'),
     path('saved_playlists/', views.get_user_saved_playlists, name='saved_playlists'),
     path('public_playlists/', views.get_all_shared_playlists, name='public_playlists'),
-    path('to_playlist/<int:playlist_id>/add_track/<int:track_id>/', views.add_track_to_playlist, name='add_track_to_playlist'),
+    path('playlist/<int:playlist_id>/tracks/', views.playlist_tracks),
+    path('<int:playlist_id>/add/', views.add_track),
+    path('move-track/', views.move_track_in_playlist),
 ]
