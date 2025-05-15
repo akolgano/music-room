85e168a5c3eb2bb4ff109a1d6274929474d35531
akolgano <anna080688@gmail.com>
2025-05-15 12:03:38 +0800

    move tracks in playlist api

--- apps/playlists/admin.py
+++ apps/playlists/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
 
-from .models import Playlist
+from .models import Playlist, PlaylistTrack
+from apps.tracks.models import Track
 
-admin.site.register(Playlist)
+class PlaylistTrackInline(admin.TabularInline):
+    model = PlaylistTrack
+    extra = 1
+    ordering = ['position']
+    readonly_fields = ['position']
+    autocomplete_fields = ['track']
+    fields = ['playlist', 'track', 'position']
+    show_change_link = True
+
+    def track_display(self, obj):
+        return f"{obj.track.name} – {obj.track.artist}"
+    track_display.short_description = "Track"
+
+class PlaylistAdmin(admin.ModelAdmin):
+    list_display = ['name', 'id']
+    inlines = [PlaylistTrackInline]
+
+class PlaylistTrackAdmin(admin.ModelAdmin):
+    list_display = ['playlist', 'position', 'track_name', 'track_artist']
+    ordering = ['playlist', 'position']
+    list_filter = ['playlist']
+    search_fields = ['track__name', 'track__artist']
+
+    def track_name(self, obj):
+        return obj.track.name
+
+    def track_artist(self, obj):
+        return obj.track.artist
+
+admin.site.register(Playlist, PlaylistAdmin)
+admin.site.register(PlaylistTrack, PlaylistTrackAdmin)
