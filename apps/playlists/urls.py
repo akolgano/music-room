8bb5ecbb6fd7d95247a1748415a9af4e6790fbb6
akolgano <anna080688@gmail.com>
2025-05-24 20:59:58 +0800

    full info broadcast in add/move/delete track

--- apps/playlists/urls.py
+++ apps/playlists/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('playlists', views.create_new_playlist, name='playlists'),
     path('playlists/<int:playlist_id>', views.get_playlist_info, name='get_playlist'), 
     path('playlists/<int:playlist_id>/tracks', views.add_items_to_playlist, name='add_items'),
-    path('playlists/<int:playlist_id>/remove_tracks', views.remove_playlist_items, name='remove_items'),
+    path('playlists/<int:playlist_id>/remove_tracks', views.delete_track_from_playlist, name='remove_items'),
     # do not need ?
     #path('save_playlist/', views.save_shared_playlist, name='save_playlist'),
     path('saved_playlists/', views.get_user_saved_playlists, name='saved_playlists'),
