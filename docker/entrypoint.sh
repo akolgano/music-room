1294cb9571a50c4baab3fcec4067d45a574e067c
akolgano <anna080688@gmail.com>
2025-05-26 17:59:19 +0800

    sample data

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,6 +14,8 @@ export PYTHONPATH=/app
 echo "Applying migrations..."
 python manage.py makemigrations
 python manage.py migrate
+echo "Loading data..."
+python manage.py loaddata sample.json
 
 SUPERUSER_EXISTS=$(python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists())")
 if [ "$SUPERUSER_EXISTS" = "False" ]; then
@@ -30,5 +32,5 @@ fi
 
 echo "Starting development server..."
 #exec python manage.py runserver 0.0.0.0:8000
-#exec daphne -b 0.0.0.0 -p 8000 core.asgi:application
-exec uvicorn core.asgi:application --reload --host 0.0.0.0 --port 8000
+exec daphne -b 0.0.0.0 -p 8000 core.asgi:application
+#exec uvicorn core.asgi:application --reload --host 0.0.0.0 --port 8000
