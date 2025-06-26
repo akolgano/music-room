dd99a696327c9b39a3c602891ade3d2f621a3b2d
akolgano <anna080688@gmail.com>
2025-06-26 19:02:26 +0800

    delete devices app

--- apps/devices/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import register_device, delegate_control, check_control_permission
-
-urlpatterns = [
-    path('register/', register_device, name='device-register'),
-    path('delegate/', delegate_control, name='delegate-control'),
-    path('<str:device_uuid>/can-control/', check_control_permission, name='check-control-permission'),
-]
