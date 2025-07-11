c6df220ae5839e02689673c371dd543757b9b579
Sian Soon yee <soonyee80@gmail.com>
2025-07-11 17:59:38 +0800

    add spectacular with swagger

--- core/urls.py
+++ core/urls.py
@@ -18,20 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 from rest_framework import permissions
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Music Room API",
-        default_version='v1',
-        description="API documentation",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 
 urlpatterns = [
@@ -42,7 +30,8 @@ urlpatterns = [
     path('playlists/', include('apps.playlists.urls')),
     path('auth/', include('apps.remote_auth.urls')),
     path('profile/', include('apps.profile.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
