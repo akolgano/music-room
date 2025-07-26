9288e61bfc786340242f72ec1355c081ed502f3d
Sian Soon yee <soonyee80@gmail.com>
2025-07-26 22:03:20 +0800

    Fix test_check_email_incorrect_user_model

--- apps/users/tests/test_check_email.py
+++ apps/users/tests/test_check_email.py
@@ -1,9 +1,11 @@
 import pytest
 from rest_framework.test import APIClient
-from django.contrib.auth.models import User
 from django.urls import reverse
 from unittest.mock import patch
 from apps.remote_auth.models import SocialNetwork
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 @pytest.mark.django_db
